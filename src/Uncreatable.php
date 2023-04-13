82b881d5c137326b30218c083c2f98e7c4260782
Alberto <alberto-bottarini@users.noreply.github.com>
2023-04-13 09:14:29 +0200

    Update Uncreatable.php

--- src/Uncreatable.php
+++ src/Uncreatable.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 trait Uncreatable
 {
 
-    public static function authorizedToCreate(Request $request)
+    public function authorizedToCreate(Request $request)
     {
         return false;
     }
