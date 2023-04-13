b6bfe02d9540ee5dbbe7e91c1b0ff618d8fad05d
Alberto <alberto-bottarini@users.noreply.github.com>
2023-04-13 10:26:27 +0200

    Update Unreplicable.php

--- src/Unreplicable.php
+++ src/Unreplicable.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 trait Unreplicable
 {
 
-    public static function authorizedToReplicate(Request $request)
+    public function authorizedToReplicate(Request $request)
     {
         return false;
     }
